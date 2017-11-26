01be49a00b6a2c83f49416e692fafa7fe5ebf768
Jose Antonio Alvarez Nieto <josete1409@gmail.com>
2017-11-26 14:31:22 +0100

    Commit Inicial

---
+++ Laberinto/src/Laberinto.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : Laberinto.cpp
+// Author      : Jose Antonio Alvarez Nieto
+// Version     :
+// Description : Proyecto final de Fundamento de Programación
+//				 Creación de un "juego" en el que a través de un laberinto tendrá que ir capturando
+//				 diferentes objetivos para ir sumando puntos. Se registrarán usuarios y sus puntuaciones
+//				 máximas que seran guardados en ficheros.
+//============================================================================
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+
+
+
+	return 0;
+}
